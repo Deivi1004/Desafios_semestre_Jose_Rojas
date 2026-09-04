480f6a8b94d813ac25f83f658cfc384966d46772
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-09-03 22:08:44 -0400

    JAVA-011 - Agregar menu con while

--- src/Main.java
+++ src/Main.java
@@ -17,9 +17,9 @@ public class Main {
         Estudiante estudiante = new Estudiante(nombre, carrera, edad);
 
 
-        if (edad < 18){
+        if (edad < 18) {
             System.out.println("Estudiante menor de edad");
-        } else if (edad < 25){
+        } else if (edad < 25) {
             System.out.println("Estudiante joven");
         } else {
             System.out.println("Estudiante adulto");
@@ -28,14 +28,32 @@ public class Main {
         estudiante.mostrarInformacion();
 
 
-        for (int i = 1; i <= 5; i++){
+        for (int i = 1; i <= 5; i++) {
             System.out.println("Procesando estudiante" + i);
         }
 
 
+        int opcion = -1;
+        while (opcion != 0) {
 
+
+            System.out.println("=== SISTEMA DUOC ===");
+            System.out.println("1. Mostrar estado");
+            System.out.println("2. Procesar operación");
+            System.out.println("0. Salir");
+
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+
+            if (opcion == 1) {
+                System.out.println("Sistema operativo.");
+            } else if (opcion == 2) {
+                System.out.println("Procesando operación...");
+            } else if (opcion == 0) {
+                System.out.println("Cerrando sistema...");
+            }
+
+        }
 
     }
-
-
 }
